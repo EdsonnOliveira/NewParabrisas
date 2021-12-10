820a9ff01ec0c9cfdb2b88fd5594ecd5adc1f8df
EdsonnOliveira <72781591+EdsonnOliveira@users.noreply.github.com>
2021-12-10 14:45:48 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,165 @@
+<html>
+    <head>
+        <title>New Parabrisas</title>
+        <?php include("PHP/meta.php");?>
+        <link rel="stylesheet" type="text/css" href="CSS/index.css">
+    </head>
+    <script type="text/javascript">
+        (function () {
+            var options = {
+                whatsapp: "+5561986296334",
+                call_to_action: "Faça seu orçamento.",
+                position: "right",
+                pre_filled_message: "Olá, quero fazer meu orçamento.",
+            };
+            var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+            var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+            s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+            var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+        })();
+    </script>
+    <body>
+        <main>
+            <div class='background'>
+                <div>
+                    <h1 class='txtBranco txt600'>Leve seu veículo à<br>outro patamar</h1>
+                    <h4 class='txtBranco txt300'>O serviço certo para o seu veículo</h4>
+                </div>
+                <!-- <div>
+                    <h1 class='txtPreto txtCentro txt600'>Entre em contato</h1>
+                    <form method='post'>
+                        <input type="text" name='nomeForm' class='input ip100 txtPreto' placeholder='Nome'>
+                        <input type="email" name='emailForm' class='input ip100 txtPreto' placeholder='E-Mail'>
+                        <input type="text" name='telefoneForm' class='input ip100 txtPreto' placeholder='Telefone'>
+                        <textarea name="mensagemForm" class='input ip100 txtPreto' placeholder='Mensagem' style='margin-bottom: 15px'></textarea>
+                        <input type="submit" name='btForm' class='button btPadrao bt100 txtBranco txt500 txtMaiusculo' value='Enviar'>
+                    </form>
+                </div> -->
+                <?php include('PHP/header.php'); ?>
+            </div>
+            <h1 class='txtTitulo txtPreto txt6100 txtCentro'>Qualidade de vidro impecável</h1>
+            <section id='qualidade'>
+                <div>
+                    <article>
+                        <h1 class='txtBranco txt500'>Isolante Térmico</h1>
+                    </article>
+                    <article>
+                        <h1 class='txtBranco txt500'>Resistente</h1>
+                    </article>
+                </div>
+                <div>
+                    <article>
+                        <h1 class='txtBranco txt500'>Segurança</h1>
+                    </article>
+                    <article>
+                        <h1 class='txtBranco txt500'>Produto Certificado</h1>
+                    </article>
+                </div>
+            </section>
+            <div class='linha'></div>
+            <h1 class='txtTitulo txtPreto txt6100 txtCentro'>Nossas Soluções</h1>
+            <section id='solucoes'>
+                <div>
+                    <article>
+                        <div>
+                            <img src="IMG/Custom/visao.png" alt="visao">
+                        </div>
+                        <div>
+                            <h1 class='txtPreto txt600 txtCentro'>Visão</h1>
+                        </div>
+                        <div>
+                            <h3 class='txtPreto txt400 txtCentro'>Ser um grupo competitivo que atue de forma abrangente no segmento automotivo, através de um portfólio de produtos com qualidade, representado por marcas fortes, com características e propostas únicas.</h3>
+                        </div>
+                    </article>
+                    <article>
+                        <div>
+                            <img src="IMG/Custom/missao.png" alt="missao">
+                        </div>
+                        <div>
+                            <h1 class='txtPreto txt600 txtCentro'>Missão</h1>
+                        </div>
+                        <div>
+                            <h3 class='txtPreto txt400 txtCentro'>Projetar e renovar os melhores para-brisas aos nossos clientes, levando a melhor experiência automotiva aos motoristas que virão a usufruir de nossos produtos.</h3>
+                        </div>
+                    </article>
+                    <article>
+                        <div>
+                            <img src="IMG/Custom/valor.png" alt="valor">
+                        </div>
+                        <div>
+                            <h1 class='txtPreto txt600 txtCentro'>Valores</h1>
+                        </div>
+                        <div>
+                            <ul>
+                                <li class='txtPreto txt400 txtCentro'>Agir de forma correta</li>
+                                <li class='txtPreto txt400 txtCentro'>A vida em primeiro lugar</li>
+                                <li class='txtPreto txt400 txtCentro'>Valorizar quem faz a nossa empresa</li>
+                                <li class='txtPreto txt400 txtCentro'>Crescer e evoluir juntos</li>
+                                <li class='txtPreto txt400 txtCentro'>Cuidar do nosso planeta</li>
+                                <li class='txtPreto txt400 txtCentro'>Fazer acontecer</li>
+                            </ul>
+                        </div>
+                    </article>
+                </div>
+            </section>
+            <section id="numeros">
+                <div>
+                    <h1 class='txtPreto txtCentro txt500'>Nossa empresa em<br>números!</h1>
+                    <div>
+                        <article>
+                            <h1 class='txtCentro txtPreto txt600'>1.5K</h1>
+                            <h2 class='txtCentro txtPreto txt500'>clientes<br>satisfeitos</h2>
+                        </article>
+                        <article>
+                            <h1 class='txtCentro txtPreto txt600'>+700</h1>
+                            <h2 class='txtCentro txtPreto txt500'>veículos<br>renovados</h2>
+                        </article>
+                    </div>
+                </div>
+                <div>
+                    <img src="IMG/all.png" alt="">
+                </div>
+            </section>
+            <div class='linha'></div>
+            <section id='clientes'>
+                <h1 class='txtCentro txtPreto txt500'>Veja o que nossos clientes<br>estão falando!</h1>
+                <div>
+                    <article>
+                        <div>
+                            <h3 class='txtPreto txtCentro txt500'>
+                                Serviço super rápido, vendedor prestativo. Marcio foi super atencioso e trocou o meu para-brisa na minha frente, me explicando cada etapa!
+                            </h3>
+                        </div>
+                        <div>
+                            <h1 class='txtPreto txtCentro txt700'>Carlos</h1>
+                            <h4 class='txtPreto txtCentro txt500'>Taguatinga</h4>
+                        </div>
+                    </article>
+                    <article>
+                        <div>
+                            <h3 class='txtPreto txtCentro txt500'>
+                                Ótima assistência, ótimo atendimento e preço firmeza. Recomendo!
+                            </h3>
+                        </div>
+                        <div>
+                            <h1 class='txtPreto txtCentro txt700'>Regina</h1>
+                            <h4 class='txtPreto txtCentro txt500'>Ceilândia</h4>
+                        </div>
+                    </article>
+                    <article>
+                        <div>
+                            <h3 class='txtPreto txtCentro txt500'>
+                                Ótima experiência, loja excelente! Entregaram meu Honda Civic de forma impecável.
+                            </h3>
+                        </div>
+                        <div>
+                            <h1 class='txtPreto txtCentro txt700'>Marcos</h1>
+                            <h4 class='txtPreto txtCentro txt500'>Águas Claras</h4>
+                        </div>
+                    </article>
+                </div>
+            </section>
+        </main>
+        <?php include('PHP/footer.php'); ?>
+    </body>
+</html>
